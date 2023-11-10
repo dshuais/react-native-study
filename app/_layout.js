8e1f386ece361fc8d0a13695c108000e4f7cb322
dshuais <1137896420@qq.com>
2023-11-10 17:44:04 +0800

    feat：新增_layout and index和依赖

---
+++ app/_layout.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: dushuai
+ * @Date: 2023-11-10 11:52:21
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-11-10 17:37:37
+ * @description: Layout
+ */
+import { Stack } from 'expo-router'
+
+const Layout = () => {
+  return <Stack />
+}
+
+export default Layout
---
+++ app/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: dushuai
+ * @Date: 2023-11-10 11:52:21
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-11-10 17:38:43
+ * @description: Home
+ */
+import { View, Text } from 'react-native'
+
+const Home = () => {
+
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+export default Home
